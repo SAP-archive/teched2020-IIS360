962bc99b4185f16b6be2f3d5b3e7823d4d6ffb32
Kolbowski, Christian <christian.kolbowski@sap.com>
2020-11-02 11:21:55 +0100

    custom page artifacts renamed

--- app/test-resources/ext/controller/ProcessFlow.controller.js
+++ app/test-resources/ext/controller/ProcessFlow.controller.js
@@ -1,36 +1,33 @@
-//beta example code. This might change in the future
+//the UI5_184 view controller pair has to be used for UI5 version >= 1.84
 sap.ui.define([
-	'jquery.sap.global',
 	'sap/ui/core/mvc/Controller',
+	'sap/fe/core/controllerextensions/RoutingListener',
 	'sap/ui/model/json/JSONModel',
-	'sap/m/MessageToast',
-	'sap/fe/core/controllerextensions/FlexibleColumnLayout',
-	'sap/fe/core/controllerextensions/Routing'
+	'sap/m/MessageToast'
 ],
-function (jQuery, Controller, JSONModel, MessageToast, FCLExtension, RoutingExtension) {
+function (Controller, RoutingListener, JSONModel, MessageToast) {
 	"use strict";
 
-	return Controller.extend("sap.fe.demo.incidents.ext.controller.ProcessFlow", {
+	return Controller.extend("myController", {
+				routingListener: RoutingListener,
+
 		constructor: function () {
 			Controller.apply(this, arguments);
-			this.routing = new RoutingExtension();
-			this.fcl = new FCLExtension();
+			// No need to instantiate the extension, it's done automatically
 		},
 
 		onInit: function () {
 			var oView = this.getView();
-			this.routing.configure(this.getView(), this.fcl);
-			this.fcl.configure(this.getView(), this.routing);
 			this.oProcessFlow1 = oView.byId("processflow1");
 
-            var sDataPath = jQuery.sap.getModulePath("sap.fe.demo.incidents.ext.controller", "/ProcessFlowLanesAndNodes.json");
-            var oModelPf1 = new JSONModel(sDataPath);
-            oView.setModel(oModelPf1, "ProcessFlowModel");
-            oModelPf1.attachRequestCompleted(this.oProcessFlow1.updateModel.bind(this.oProcessFlow1));
-        },
+			var sDataPath = jQuery.sap.getModulePath("sap.fe.demo.incidents.ext.controller", "/ProcessFlowLanesAndNodes.json");
+			var oModelPf1 = new JSONModel(sDataPath);
+			oView.setModel(oModelPf1, "ProcessFlowModel");
+			oModelPf1.attachRequestCompleted(this.oProcessFlow1.updateModel.bind(this.oProcessFlow1));
+		},
 
-        onNodePress: function (event) {
-            MessageToast.show("Node " + event.getParameters().getNodeId() + " has been clicked.");
-        }
-    });
+		onNodePress: function (event) {
+			MessageToast.show("Node " + event.getParameters().getNodeId() + " has been clicked.");
+		}
+	});
 });
